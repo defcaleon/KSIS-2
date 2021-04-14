fc9f823343abe339fae39066817a5bfff63ea095
Nikita Parmon <nikitka.parmon@mail.ru>
2021-04-14 19:58:46 +0300

    init commit

---
+++ src/website/com/Main.java
@@ -0,0 +1,114 @@
+package website.com;
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main extends JFrame {
+
+    private JTextArea textArea;
+    private JTextField textField;
+
+    private final String FRM_TITLE= "Chat";
+    private final int FRM_LOC_X=100;
+    private final int FRM_LOC_Y=100;
+    private final int FRM_WIDTH=400;
+    private final int FRM_HEIGHT=400;
+
+    private final int PORT = 9876;
+    private final String IP_BROADCAST="192.168.0.255";
+
+
+    private class thdReciever extends  Thread{
+        @Override
+        public void start(){
+            super.start();
+            try {
+                cuztomize();
+            }catch (Exception ex){
+                ex.printStackTrace();
+            }
+
+        }
+        private void cuztomize() throws Exception{
+            DatagramSocket recieveSocket = new DatagramSocket(PORT);
+            Pattern regex = Pattern.compile("[\u0020-\uFFFF]");
+
+            while(true){
+                byte[] receiveData = new byte[1024];
+                DatagramPacket recievePacket= new DatagramPacket(receiveData,receiveData.length);
+                recieveSocket.receive(recievePacket);
+                InetAddress IPAddress = recievePacket.getAddress();
+                int port = recievePacket.getPort();
+
+                String sentence = new String(recievePacket.getData());
+                Matcher m = regex.matcher(sentence);
+
+                textArea.append(IPAddress.toString()+":"+port+": ");
+                while (m.find()){
+                    textArea.append(sentence.substring(m.start(),m.end()));
+                }
+                textArea.append("\r\n");
+                textArea.setCaretPosition(textArea.getText().length());
+            }
+        }
+    };
+
+
+    private void btnSend_Handler() throws Exception{
+        DatagramSocket sendSocket = new DatagramSocket();
+        InetAddress IPAddress = InetAddress.getByName(IP_BROADCAST);
+        byte[] sendData;
+        String sentence = textField.getText();
+        textField.setText("");
+        sendData=sentence.getBytes(StandardCharsets.UTF_8);
+        DatagramPacket sendPacket = new DatagramPacket(sendData,sendData.length,IPAddress,PORT);
+        sendSocket.send(sendPacket);
+    }
+
+    private void frameDraw(JFrame frame){
+        textArea = new JTextArea(FRM_HEIGHT/19,50);
+        textField = new JTextField();
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        scrollPane.setLocation(0,0);
+        textArea.setLineWrap(true);
+        textArea.setEditable(false);
+
+        JButton btnSend = new JButton();
+        btnSend.setText("Send");
+        btnSend.setToolTipText("Broadcast a message");
+        btnSend.addActionListener(e->{
+
+            try{
+                btnSend_Handler();
+            }catch (Exception ex){
+                ex.printStackTrace();
+            }
+        });
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setTitle(FRM_TITLE);
+        frame.setLocation(FRM_LOC_X,FRM_LOC_Y);
+        frame.setSize(FRM_WIDTH,FRM_HEIGHT);
+        frame.setResizable(false);
+        frame.getContentPane().add(BorderLayout.NORTH, scrollPane);
+        frame.getContentPane().add(BorderLayout.CENTER,textField);
+        frame.getContentPane().add(BorderLayout.EAST,btnSend);
+        frame.setVisible(true);
+    }
+
+    private void antistatic(){
+        frameDraw(new Main());
+        new thdReciever().start();
+    }
+
+    public static  void main(String[] args){
+
+       new Main().antistatic();
+
+    }
+}
